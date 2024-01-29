35e79b9f6acc841f7eb837a40674f9932a73b2c3
workhome <stanchikzt@gmail.com>
2024-01-29 22:58:20 +0200

    refactor all

--- api.py
+++ api.py
@@ -1,8 +1,16 @@
-class ApiApp(MDApp):
+import requests
+
+
+API_KEY = "f7921f10dc460b8800faf0500fb1bb48"
+
+
+class ApiApp:
+
     def get_weather(self, city_name):
+
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}"
+
         try:
-            api_key = "f7921f10dc460b8800faf0500fb1bb48"
-            url = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}"
             response = requests.get(url)
             x = response.json()
             print(x)
@@ -11,15 +19,14 @@ class ApiApp(MDApp):
                 humidity = x["main"]["humidity"]
                 id = str (x["weather"][0]["id"])
                 wind_speed = round(x["wind"]["speed"]*18/5)
-                self.root.ids.temperature.text = f"[b]{temperature}[/b]°"
-                self.root.ids.humidity.text = f"{humidity}%"
-                self.root.ids.wind_speed.text = f"{wind_speed} km/h"
-                
-                          
-                
-            
+                # self.root.ids.temperature.text = f"[b]{temperature}[/b]°"
+                # self.root.ids.humidity.text = f"{humidity}%"
+                # self.root.ids.wind_speed.text = f"{wind_speed} km/h"
+                return temperature, humidity, wind_speed
             else:
                 print('Місто не знайдено!')
+                return None, None, None
         
         except requests.ConnectionError:
             print("Немає з'єднання!")
+            return None, None, None
--- weather.py
+++ weather.py
@@ -1,94 +1,29 @@
-from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.button import Button 
 from kivymd.app import MDApp
-from kivymd.uix.screen import Screen
 from kivy.lang import Builder
 from kivymd.uix.screen import MDScreen
-import requests
-import ApiApp from api.py
+from api import ApiApp
+
+Builder.load_file('weather.kv')
 
 
-
-username_helper = '''
-MDFloatLayout:
-    orientation: 'vertical'
-    size_hint: 0.9, 0.9
-       
-    MDTextField:
-        hint_text: "Місто"
-        pos_hint: {"center_x": 0.5, "center_y": 0.9}        
-        size_hint_x:None        
-        width:800         
-        multiline: True
-        mode: "fill"
-        fill_color: 0, 0, 0, .4
-        icon_right: "magnify"
-        on_text_validate: app.print_text(self.text)
-        
-    
-    Image:
-        source: "images//pngimg.com - sun_PNG13434.png"  
-        size_hint: .2, .2
-        pos_hint: {"center_x": 0.5, "center_y": 0.7}
-        
-    MDLabel:
-        id: temperature
-        text: "[b]40°[/b]" 
-        markup: True
-        pos_hint: {"center_x": 0.5, "center_y": 0.6}
-        halign: "center"
-        font_size: "50sp"
-    Image:
-        source: "images//219816.png"
-        pos_hint: {"center_x": 0.37, "center_y": 0.5}
-        size_hint: .1, .1
-    MDLabel:
-        id: humidity
-        text: "80%"
-        pos_hint: {"center_x": 0.9, "center_y": 0.55}
-        font_size: "25sp"
-    Image:
-        source: "images//7650299.png"
-        pos_hint: {"center_x": 0.55, "center_y": 0.5}
-        size_hint: .1, .1
-    MDLabel:
-        id: wind_speed
-        text: "80км/год"
-        pos_hint: {"center_x": 1.1, "center_y": 0.5}
-        font_size: "18sp"
-    
-    
-     
-    
-'''   
-  
-
-    
 class MyScreen(MDScreen):
-    def __init__(self, **kwargs):
-        super(MyScreen, self).__init__(**kwargs)
-        self.md_bg_color = (0.29, 1, 1, 1)
+    pass
     
        
 class WeatherApp(MDApp):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.weather = ApiApp()
+
     def build(self):
         search = MyScreen()
-        username = Builder.load_string(username_helper)
-        search.add_widget(username)
         return search
-    
-    
-   
-    
-          
-        
 
-        
-    
-    
-        
-    
+    def get_weather(self, text):
+        temperature, humidity, wind_speed = self.weather.get_weather(text)
+        print(temperature, humidity, wind_speed)
+
     
 if __name__ == "__main__":
     WeatherApp().run() 
